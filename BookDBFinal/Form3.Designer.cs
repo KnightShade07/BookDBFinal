53c625ebfd02d92ccd249096db3d27efb5882ea5
TemplarFalchion75 <ethen.oliva@yahoo.com>
2020-06-16 11:41:58 -0700

    Gave more accurate name to form, added population code for the book and object creation. Also added buttons

--- BookDBFinal/Form3.Designer.cs
+++ BookDBFinal/Form3.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace BookDBFinal
 {
-    partial class Form3
+    partial class AddBookFrm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.cbBookTitle = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbISBN = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cbPrice = new System.Windows.Forms.ComboBox();
+            this.btnDBAddBook = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // Form3
+            // cbBookTitle
+            // 
+            this.cbBookTitle.FormattingEnabled = true;
+            this.cbBookTitle.Location = new System.Drawing.Point(330, 150);
+            this.cbBookTitle.Name = "cbBookTitle";
+            this.cbBookTitle.Size = new System.Drawing.Size(121, 21);
+            this.cbBookTitle.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(279, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Book";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(279, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "ISBN";
+            // 
+            // cbISBN
+            // 
+            this.cbISBN.FormattingEnabled = true;
+            this.cbISBN.Location = new System.Drawing.Point(330, 204);
+            this.cbISBN.Name = "cbISBN";
+            this.cbISBN.Size = new System.Drawing.Size(121, 21);
+            this.cbISBN.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(279, 252);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Price";
+            // 
+            // cbPrice
+            // 
+            this.cbPrice.FormattingEnabled = true;
+            this.cbPrice.Location = new System.Drawing.Point(330, 252);
+            this.cbPrice.Name = "cbPrice";
+            this.cbPrice.Size = new System.Drawing.Size(121, 21);
+            this.cbPrice.TabIndex = 16;
+            // 
+            // btnDBAddBook
+            // 
+            this.btnDBAddBook.Location = new System.Drawing.Point(330, 306);
+            this.btnDBAddBook.Name = "btnDBAddBook";
+            this.btnDBAddBook.Size = new System.Drawing.Size(121, 64);
+            this.btnDBAddBook.TabIndex = 17;
+            this.btnDBAddBook.Text = "Add Book To Database";
+            this.btnDBAddBook.UseVisualStyleBackColor = true;
+            this.btnDBAddBook.Click += new System.EventHandler(this.btnDBAddBook_Click);
+            // 
+            // AddBookFrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form3";
+            this.Controls.Add(this.btnDBAddBook);
+            this.Controls.Add(this.cbPrice);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cbISBN);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cbBookTitle);
+            this.Name = "AddBookFrm";
             this.Text = "Form3";
             this.Load += new System.EventHandler(this.Form3_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cbBookTitle;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbISBN;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cbPrice;
+        private System.Windows.Forms.Button btnDBAddBook;
     }
 }
--- BookDBFinal/Form3.cs
+++ BookDBFinal/Form3.cs
@@ -10,16 +10,41 @@ using System.Windows.Forms;
 
 namespace BookDBFinal
 {
-    public partial class Form3 : Form
+    public partial class AddBookFrm : Form
     {
-        public Form3()
+        public AddBookFrm()
         {
             InitializeComponent();
         }
 
         private void Form3_Load(object sender, EventArgs e)
         {
+            List<Book> allBooks = BookDB.GetAllBooks();
 
+            PopulateBookList(allBooks);
+        }
+
+        private void PopulateBookList(List<Book> books)
+        {
+            foreach (Book book in books)
+            {
+                cbBookTitle.Items.Add(book.Title);
+                cbISBN.Items.Add(book.BookIBSN);
+                cbPrice.Items.Add(book.Price);
+            }
+        }
+
+        private void btnDBAddBook_Click(object sender, EventArgs e)
+        {
+            //Creates Book Object
+            Book book = new Book();
+            book.Title = cbBookTitle.Text;
+            book.BookIBSN = cbISBN.Text;
+            book.Price = Decimal.Parse(cbPrice.Text);
+
+
+            //adds to the database
+            BookDB.Add(book);
         }
     }
 }
